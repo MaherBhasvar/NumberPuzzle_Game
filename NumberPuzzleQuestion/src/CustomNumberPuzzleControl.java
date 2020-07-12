d63f7728c855694dda9dcec84129c99e1693993f
MaherBhasvar <maher.daiict@gmail.com>
2020-07-12 22:07:01 +0530

    completed

--- NumberPuzzleQuestion/src/CustomNumberPuzzleControl.java
+++ NumberPuzzleQuestion/src/CustomNumberPuzzleControl.java
@@ -28,6 +28,7 @@ class CustomNumberPuzzleControl extends NumberPuzzleControl {
 	public String getWinnerMessage() {
 		return "Congrats, you have won!";
 	}
+	
 
 	// The following three methods have to be written by the participants...
 
@@ -35,19 +36,90 @@ class CustomNumberPuzzleControl extends NumberPuzzleControl {
 		int emptyCellId = game.getEmptyCellId();
 		Button buttonClicked = game.getButtonClicked();
 		Button[] buttons = game.getButtons();
+//		System.out.println("s:"+buttonClicked.getLabel());
+		String label = buttonClicked.getLabel();
+		//Your logic here
+		int count = 0;
+		for (Button b : buttons) {
+			if (b == buttonClicked) {
+				break;
+			}
+			count++;
+		}
+		swapButton(buttons[game.getEmptyCellId()], buttonClicked);
 		
-		//Your logic here		
+		game.setEmptyCellId(count);
 		
-		return emptyCellId;
+		return count;
 
 	}
+	
+	public int ConvertS2I (String s) {
+		System.out.println("s:"+s);
+		switch (s) {
+		case "1":
+			return 1;
+		case "2":
+			return 2;
+		case "3":
+			return 3;
+		case "4":
+			return 4;
+		case "5":
+			return 5;
+		case "6":
+			return 6;
+		case "7":
+			return 7;
+		case "8":
+			return 8;
+		case "9":
+			return 9;
+		case "10":
+			return 10;
+		case "11":
+			return 11;
+		case "12":
+			return 12;
+		case "13":
+			return 13;
+		case "14":
+			return 14;
+		case "15":
+			return 15;
+		default:
+			return 0;
+		}
+	}
+	
 	public int[] getRandomNumbersForGrid() {
 		int arr[] = new int[15];
 		
 		//Your logic here
-		int a = getRandomNumber();
+//		int a = getRandomNumber();
+		int count = 0;
 		
-		
+		while (count!=15) {
+			int a = (int)getRandomNumber();
+			if (a>15) {
+				a = 15;
+			}else if (a<1){
+				a = 1;
+			}
+			
+			boolean found = false;
+			for (int i = 0; i<=count; i++) {
+				if (arr[i] == a) {
+					found = true;
+					break;
+				}
+			}
+			if (found == false) {
+				arr[count] = a;
+				count++;
+			}
+		}
+
 		return arr;
 	}
 	public boolean checkForWinner(Button[] buttons)
@@ -55,8 +127,13 @@ class CustomNumberPuzzleControl extends NumberPuzzleControl {
 		boolean winner = true;
 		
 		// Your Logic here
-		getIntegerArrayOfButtonIds(buttons);
-
+		int[] arr = getIntegerArrayOfButtonIds(buttons);
+		for (int i=0; i<15; i++) {
+			if (arr[i] != i+1) {
+				winner = false;
+				break;
+			}
+		}
 		return winner;
 	}
 }
